018cbc092f25eccc04a91de3547088b8b65e9c0f
Cody Fincher <cody.fincher@gmail.com>
2022-09-04 13:55:52 -0500

    remove more code

--- src/starlite_bedrock/starlite/__init__.py
+++ src/starlite_bedrock/starlite/__init__.py
@@ -1,29 +1,10 @@
 from starlite import Starlite
 
-from starlite_bedrock.starlite.controller import Controller
-from starlite_bedrock.starlite.handlers import (
-    delete,
-    get,
-    get_collection,
-    patch,
-    post,
-    put,
-)
 from starlite_bedrock.starlite.response import Response
 
 __all__ = [
-    "Controller",
     "Starlite",
     "Response",
-    "delete",
-    "dependencies",
     "exceptions",
-    "filter_parameters",
-    "get",
-    "get_collection",
-    "guards",
     "logging",
-    "patch",
-    "post",
-    "put",
 ]
